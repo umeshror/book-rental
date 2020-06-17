d00ae2cae8b3442aac25c667cd5644b238049fe9
umeshror <sarukumesh@gmail.com>
2020-06-17 18:01:36 +0530

    factories added

---
+++ apps/book_rental/tests/factories.py
@@ -0,0 +1,53 @@
+import django
+from factory.fuzzy import FuzzyChoice
+
+django.setup()
+
+import factory
+from django.contrib.auth.models import User
+from factory.django import DjangoModelFactory
+
+from apps.book_rental.models import Category, Book, RentedBook
+
+
+class UserFactory(DjangoModelFactory):
+    class Meta:
+        model = User
+        django_get_or_create = ('username',)
+
+    username = factory.Sequence(lambda n: 'user_{}'.format(n))
+    first_name = factory.Sequence(lambda n: 'first_{}'.format(n))
+    last_name = factory.Sequence(lambda n: 'last_{}'.format(n))
+    email = factory.LazyAttribute(lambda a: '{0}.{1}@example.com'.format(a.first_name, a.last_name).lower())
+    password = "pass123"
+
+
+class CategoryFactory(DjangoModelFactory):
+    name = factory.fuzzy.FuzzyChoice(["Fiction", "General", "Novels", "Horror"])
+    created_by = factory.SubFactory(UserFactory)
+
+    class Meta:
+        model = Category
+        django_get_or_create = ('name',)
+
+
+class BookFactory(DjangoModelFactory):
+    name = factory.Sequence(lambda n: 'name_{}'.format(n))
+    description = factory.Sequence(lambda n: 'description_{}'.format(n))
+
+    book_quantity = factory.Sequence(lambda n: n)
+    category = factory.SubFactory(CategoryFactory)
+    author = factory.SubFactory(UserFactory)
+    created_by = factory.SubFactory(UserFactory)
+
+    class Meta:
+        model = Book
+        django_get_or_create = ('name',)
+
+
+class RentedBookFactory(DjangoModelFactory):
+    book = factory.SubFactory(BookFactory)
+    created_by = factory.SubFactory(UserFactory)
+
+    class Meta:
+        model = RentedBook
