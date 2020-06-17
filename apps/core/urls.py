811ba9e307b6e3caaa068ab7f0030bbdaffe9e18
umeshror <sarukumesh@gmail.com>
2020-06-17 18:00:58 +0530

    urls updated

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -3,5 +3,5 @@ from rest_framework import routers
 from apps.core.views import UserViewSet
 
 router = routers.DefaultRouter()
-router.register(r'user', UserViewSet, basename='users')
+router.register(r'', UserViewSet, basename='users')
 urlpatterns = router.urls
