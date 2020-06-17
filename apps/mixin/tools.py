5e807f59c9d266a955b9694fc703c037a3394e94
umeshror <sarukumesh@gmail.com>
2020-06-17 06:49:50 +0530

    query_debugger added

---
+++ apps/mixin/tools.py
@@ -0,0 +1,28 @@
+from django.db import connection, reset_queries
+import time
+import functools
+
+
+def query_debugger(func):
+    """
+    Decorator used for query optimisation
+    Gives DB hits for a method
+    """
+
+    @functools.wraps(func)
+    def inner_func(*args, **kwargs):
+        reset_queries()
+
+        start_queries = len(connection.queries)
+
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+
+        end_queries = len(connection.queries)
+        print(f"Function : {func.__name__}")
+        print(f"Number of Queries : {end_queries - start_queries}")
+        print(f"Finished in : {(end - start):.2f}s")
+        return result
+
+    return inner_func
