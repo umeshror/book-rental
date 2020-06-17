f6ad793fd15b036319626f2aa87dfabe88a7a12e
umeshror <sarukumesh@gmail.com>
2020-06-17 07:04:44 +0530

    docker conf added

---
+++ docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# Collect static files
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Database migrations"
+python manage.py migrate --noinput
+
+gunicorn config.wsgi:application --bind 0.0.0.0:8000
+
